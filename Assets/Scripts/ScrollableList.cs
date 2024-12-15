e548c66914dc8083b4710bb377243e7a511c5abd
Johan <50304866+JohanMJK@users.noreply.github.com>
2024-12-15 08:17:09 +0100

    Updates

--- Assets/Scripts/ScrollableList.cs
+++ Assets/Scripts/ScrollableList.cs
@@ -6,10 +6,7 @@ using UnityEngine.UI;
 public class ScrollableList : MonoBehaviour
 {
     public string systemName;
-    public UnityEngine.UI.Button listItemPrefabGrey;
-    public UnityEngine.UI.Button listItemPrefabYellow;
-    public UnityEngine.UI.Button listItemPrefabRed;
-    public GameObject dataBoxPrefab;
+    public UnityEngine.UI.Button listItemPrefab;
     public Transform contentParent;
 
 
@@ -21,29 +18,14 @@ public class ScrollableList : MonoBehaviour
     private async void PopulateLists()
     {
         await DatabaseManager.PopulateListsDone();
-
-        VerticalLayoutGroup contentContainer = contentParent.GetComponent<VerticalLayoutGroup>();
         try
         {
             foreach (var container in DatabaseManager.LiveContainers)
             {
                 if (container.SystemName == systemName)
                 {
-                    Button instantiatedButton = Instantiate(listItemPrefabGrey, contentParent.transform);
-                    //if (container.AlarmActive)
-                    //{
-                    //    instantiatedButton = Instantiate(listItemPrefabRed, contentParent.transform);
-
-                    //}
-                    //else if (container.NumAlarmsAboveLimit)
-                    //{
-                    //    instantiatedButton = Instantiate(listItemPrefabYellow, contentParent.transform);
-                    //}
-                    //else
-                    //{
-                    //    instantiatedButton = Instantiate(listItemPrefabGrey, contentParent.transform);
-                    //}
-                    instantiatedButton.GetComponentInChildren<TextMeshProUGUI>().text = container.Description; ;
+                    Button instantiatedButton = Instantiate(listItemPrefab, contentParent.transform);
+                    instantiatedButton.GetComponentInChildren<TextMeshProUGUI>().text = container.Description;
                     instantiatedButton.GetComponent<ListItemPrefabController>().topicContainer = container;
                 }
             }
